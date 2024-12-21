ccdaca06da893e3986e2843b2ab78775c027f9c5
Dmitry Korablin <korablindm@gmail.com>
2024-12-22 00:14:07 +0300

    deldata DB functions

--- src/managedb/deldata.go
+++ src/managedb/deldata.go
@@ -2,15 +2,16 @@ package managedb
 
 // Comments related to post will be deleted automatically
 func (db *DB) DeletePostById(id uint64) error {
-	
-	
-	return nil
+	_, err := db.Database.Exec("DELETE FROM Post WHERE Id = $1", id)
+	return err
 }
 
 func (db *DB) DeleteCommentById(id uint64) error {
-	return nil
+	_, err := db.Database.Exec("DELETE FROM Comment WHERE Id = $1", id)
+	return err
 }
 
 func (db *DB) DeleteUnusedTags() error {
-	return nil
+	_, err := db.Database.Exec("DELETE FROM TagText WHERE Id NOT IN (SELECT DISTINCT TagId FROM Tags)")
+	return err
 }
